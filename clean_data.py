783d2e62e9acc067be7daa25e89e543d2f245d42
HoangCau <hoangcau14783@gmail.com>
2023-12-21 21:05:39 +0700

    Update clean_data.py - divide Date Column in dataset into Day, MOnth, Year

--- clean_data.py
+++ clean_data.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Đọc dữ liệu từ file Excel
 xl_file = pd.ExcelFile('supermarket_sales.xls')
-df = xl_file.parse('supermarket_sales')  # Chọn sheet 'SupermarketSales'
+df = xl_file.parse('supermarket_sales')  # Chọn sheet 'supermarket_sales'
 
 # Clean cột "Gender"
 df['Gender'] = df['Gender'].apply(lambda x: 'Female' if x in ['F', 'Female'] else 'Male' if x in ['M', 'Male'] else x)
@@ -17,9 +17,23 @@ def parse_date(x):
 
 df['Date'] = df['Date'].apply(parse_date)
 
+# Chia cột "Date" thành các cột "Day", "Month", "Year"
+df['Day'] = pd.to_datetime(df['Date'], format='%d/%m/%Y').dt.day
+df['Month'] = pd.to_datetime(df['Date'], format='%d/%m/%Y').dt.month
+df['Year'] = pd.to_datetime(df['Date'], format='%d/%m/%Y').dt.year
+
+# Sắp xếp lại các cột theo thứ tự mong muốn
+desired_order = [
+    'Invoice ID', 'Branch', 'Customer type', 'Gender', 'ProductID', 'Quantity', 'Tax 5%', 'Total',
+    'Day', 'Month', 'Year', 'Time', 'Payment', 'cogs', 'gross margin percentage',
+    'gross income', 'Rating'
+]
+
+df = df[desired_order]
+
 # Lưu dữ liệu đã làm sạch và giữ lại các sheet khác
 with pd.ExcelWriter('SupermarketSales_Cleaned.xlsx') as writer:
-    df.to_excel(writer, index=False, sheet_name='supermarket_sales')  # Lưu sheet 'SupermarketSales'
+    df.to_excel(writer, index=False, sheet_name='supermarket_sales')  # Lưu sheet 'supermarket_sales'
     for sheet_name in xl_file.sheet_names:
         if sheet_name != 'supermarket_sales':
             sheet_data = xl_file.parse(sheet_name)
